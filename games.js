c10488f63a2d2e93993b6df09f67840fc736cd99
Glitch (powerschool-log) <none>
2022-05-05 16:52:58 +0000

    ☺️🙉 Checkpoint ./games.js:38512448/462 ./games.html:38512448/35

--- games.js
+++ games.js
@@ -2,17 +2,15 @@ var pagetest = document.referrer;
 if ( pagetest ==  "https://powerschool-log.glitch.me/gameslogin.html") 
     //checks if the last used website is the password website 
     {
-      alert("yes");
+    
     }
 if ( pagetest !=  "https://powerschool-log.glitch.me/gameslogin.html") 
     //checks if the last used website is the password website 
     {
-      alert("no");
+
       window.location.href = "https://powerschool-log.glitch.me/testing.html";
+    
     }
- var password = document.getElementById("passwordBox");
-    var passwordText = password.value;
-document.getElementById("p1").innerHTML =  passwordtext;
 
 
 
