60b18afcfa977282979f042de788d13c6579c59e
hackerman103020 <94463996+hackerman103020@users.noreply.github.com>
2023-11-28 19:33:19 -0600

    Update games.js

--- games.js
+++ games.js
@@ -1,8 +1,8 @@
 var pagetest = document.referrer;
-if (pagetest == "https://powerschool-log.glitch.me/gameslogin.html") {
+if (pagetest == "https://hackerman103020.github.io/PowerStudent/gameslogin.html") {
   //checks if the last used website is the password website
 }
-if (pagetest != "https://powerschool-log.glitch.me/gameslogin.html") {
+if (pagetest != "https://hackerman103020.github.io/PowerStudent/gameslogin.html") {
   //checks if the last used website is the password website
   window.location.href = "/index.html";
 }
